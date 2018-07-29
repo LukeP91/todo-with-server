9599d45abfe8eac5b03d58c919ab266d8496ace4
Luke Pawlik <lukepawlik@gmail.com>
2018-07-29 17:02:31 +0200

    add strike when item is completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { toggle, remove } from '../action_creators/todosActionCreators'
 
-let Todo = props => {
-  return (
-    <div>
-      <div onClick={() => props.toggle(props.todo.id)}>{props.todo.description}</div>
-      <button onClick={() => props.remove(props.todo.id)}>X</button>
-    </div>
-  )
+class Todo extends Component {
+  description = () => {
+    const { todo } = this.props
+
+    if (todo.completed) {
+      return <strike>{todo.description}</strike>
+    } else {
+      return <span>{todo.description}</span>
+    }
+  }
+
+  render() {
+    const { toggle, remove, todo } = this.props
+
+    return (
+      <div>
+        <div onClick={() => toggle(todo.id)}>{this.description()}</div>
+        <button onClick={() => remove(todo.id)}>X</button>
+      </div>
+    )
+  }
 }
 
 const mapDispatchToProps = {
